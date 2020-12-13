f5cee500359338dbfe507017404b04274922790a
andres <andrestati17@gmail.com>
2020-12-13 13:59:38 -0600

    arreglos en la extension de guardado

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,7 +17,15 @@ public class Conexion {
             System.out.println("TextoSuperior: " + textoSuperior);
             System.out.println("TextoInferior: " + textoInferior);
             */
-            String rutaGuardado = "C:\\Users\\ExtremeTech\\Desktop\\TEC\\4to Semestre\\Lenguajes\\ParadigmaOO\\Proyecto4\\proyectosCreados\\"+nombreProyecto+"Generado.jpg";
+
+            File postal = new File(rutaImagen);
+            String p1 = postal.getName();
+            String p2 = p1.replace(".", ",");
+            String p3[] = p2.split(",");
+            String p4 = p3[1];
+
+            String rutaGuardado = "C:\\Users\\ExtremeTech\\Desktop\\TEC\\4to Semestre\\Lenguajes\\ParadigmaOO\\Proyecto4\\proyectosCreados\\"+nombreProyecto+"Generado." + p4;
+            
             Process process = new ProcessBuilder("C:\\Users\\ExtremeTech\\CLionProjects\\Postales\\cmake-build-debug\\Postales.exe",rutaImagen,
                     tipoLetra,tamanoLetra, textoSuperior,textoInferior, color, rutaGuardado).start();
             
